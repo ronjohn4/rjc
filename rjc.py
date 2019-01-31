50f0a8a583c4d16ac3e62118315fa5517d417c71
Ron <ronjohn4@gmail>
2019-01-31 12:08:43 +0100

    Data model repurpose

--- rjc.py
+++ rjc.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import User, History
 
+
 # Registers objects for use after 'flask shell' is used from command line
 @app.shell_context_processor
 def make_shell_context():
