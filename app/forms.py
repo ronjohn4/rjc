02372fca1268bc237ba50f32e251c4e78f4ea74d
Ron <ronjohn4@gmail>
2019-03-29 14:27:46 +0100

    Added home page text

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from app.models import User
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField('uUsername', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
